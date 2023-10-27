52323bce9de1807a46f688703bc3b7ed81ef83bd
DamiánRi <dams_rive@hotmail.com>
2023-10-26 20:34:06 -0600

    Add PWA configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import './118n.js'
-import './firebase.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./118n.js";
+import "./firebase.js";
+import "./sw.js";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCircleCheck, faXmark, faSpinner, faUser, faPlus, faTriangleExclamation, faDollarSign, faArrowTrendDown, faArrowTrendUp } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    faCircleCheck,
+    faXmark,
+    faSpinner,
+    faUser,
+    faPlus,
+    faTriangleExclamation,
+    faDollarSign,
+    faArrowTrendDown,
+    faArrowTrendUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faCircleCheck, faXmark, faSpinner, faUser, faPlus, faTriangleExclamation, faDollarSign, faArrowTrendDown, faArrowTrendUp)
+library.add(
+    faCircleCheck,
+    faXmark,
+    faSpinner,
+    faUser,
+    faPlus,
+    faTriangleExclamation,
+    faDollarSign,
+    faArrowTrendDown,
+    faArrowTrendUp
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: { enabled: true },
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+            },
+            includeAssets: [
+                "favicon.ico",
+                "vite.svg",
+                "apple-touch-icon-180x180.png",
+            ],
+            manifest: {
+                name: "MisCuentas",
+                short_name: "MC",
+                description:
+                    "Aplicación para la contabilidad de gastos personales.",
+                theme_color: "#2c3e50",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
